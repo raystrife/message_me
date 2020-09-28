af7d799bdb4f8a5beb34085821bf42e06c6d7f74
raystrife <rayadrian96@yahoo.com>
2020-09-28 12:29:50 -0400

    Add a function that allow the function to post messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//=require turbolinks
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("semantic-ui")
+require("semantic-ui-sass")
 
-
-$(document).on('turbolinks:load', function() {			//dropdown is not working!
-	$('.ui.dropdown').dropdown();
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
